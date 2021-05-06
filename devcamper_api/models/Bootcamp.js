58aabaa5959cc9ffb0dc2c010c1b620fca542ec0
retrydp <thetemon@gmail.com>
2021-05-06 12:15:48 +0300

    added geocode & created location field

--- devcamper_api/models/Bootcamp.js
+++ devcamper_api/models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -35,6 +36,23 @@ const BootcampSchema = new mongoose.Schema({
     required: [true, 'Please add an address'],
   },
 
+  location: {
+    // GeoJSON Point
+    type: {
+      type: String,
+      enum: ['Point'],
+    },
+    coordinates: {
+      type: [Number],
+      index: '2dsphere',
+    },
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
   careers: {
     // Array of strings
     type: [String],
@@ -79,4 +97,23 @@ BootcampSchema.pre('save', function (next) {
   next();
 });
 
+// Geocode & create location field
+BootcampSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  // Do not save address
+  this.address = undefined;
+
+  next();
+});
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
