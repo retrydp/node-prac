7c9a85a26154bbc79bb59909ec3788fd9057daba
retrydp <thetemon@gmail.com>
2021-05-06 09:00:42 +0300

    added error handler

--- devcamper_api/controllers/bootcamps.js
+++ devcamper_api/controllers/bootcamps.js
@@ -34,9 +34,8 @@ exports.getBootcamp = async (req, res, next) => {
       data: bootcamp,
     });
   } catch (err) {
-    res.status(400).json({
-      success: false,
-    });
+    // res.status(400).json({ success: false });
+    next(err);
   }
 };
 
