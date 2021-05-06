b0d6c27389512a076a1bcab05b5476bc327571f4
retrydp <thetemon@gmail.com>
2021-05-06 09:19:30 +0300

    custom ErrorResponce class

--- devcamper_api/controllers/bootcamps.js
+++ devcamper_api/controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const Bootcamp = require('../models/Bootcamp');
+const ErrorResponce = require('../utils/errorReponce');
 
 // @desc     Get all bootcamps
 // @route    GET /api/v1/bootcamps
@@ -25,17 +26,14 @@ exports.getBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp)
-      return res.status(400).json({
-        success: false,
-      });
+      return next(new ErrorResponce(`Bootcamp not found with id of ${req.params.id}`, 404));
 
     res.status(200).json({
       success: true,
       data: bootcamp,
     });
   } catch (err) {
-    // res.status(400).json({ success: false });
-    next(err);
+    next(new ErrorResponce(`Bootcamp not found with id of ${req.params.id}`, 404));
   }
 };
 
